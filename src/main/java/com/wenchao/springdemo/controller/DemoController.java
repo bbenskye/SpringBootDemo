ff2fe9777be32bd0073bbc3bc4288f65db2659fe
muwenchao <muwenchao@vipkid.com.cn>
2019-09-17 17:18:55 +0800

    add root

--- src/main/java/com/wenchao/springdemo/controller/DemoController.java
+++ src/main/java/com/wenchao/springdemo/controller/DemoController.java
@@ -6,6 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+    @RequestMapping("/")
+    public String root(){
+        return "<html>\n" +
+                "<head>\n" +
+                "<meta charset=\"UTF-8\">\n" +
+                "<title>超哥的Spring</title>\n" +
+                "</head>\n" +
+                "\n" +
+//                "<hr />\n" +
+                "<strong>肖明亮最帅！</strong><br />" +
+                "";
+    }
+
     @RequestMapping("/login")
     public String hello(){
         return "{ \"A\": \"hello\",\"B\":\"hello 你大爷！\"}";
